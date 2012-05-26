fe7f29ffaa8303d7f33ec5e627ada236161b9f68
Daniel Wallace <daniel.wallace12@gmail.com>
2012-05-26 14:06:19 -0400

    strrchr to grab file
    strrchr to grab just the file and no more of the structure

--- pacrat.c
+++ pacrat.c
@@ -1,31 +1,4 @@
-#define _GNU_SOURCE
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <locale.h>
-#include <getopt.h>
-#include <sys/stat.h>
-
-#include <alpm.h>
-
-#ifndef PACMAN_ROOT
-	#define PACMAN_ROOT "/"
-#endif
-
-#ifndef PACMAN_DB
-	#define PACMAN_DBPATH "/var/lib/pacman"
-#endif
-
-static void copy(const char *, const char *);
-static void archive(const char *, const char *);
-static int is_modified(const char *, const alpm_backup_t *);
-static void alpm_do_backup(alpm_pkg_t *pkg);
-static void alpm_find_backups(void);
-static int parse_options(int, char*[]);
-
+#include "pacrat.h"
 /* runtime configuration */
 static struct {
 	int opmask;
@@ -63,7 +36,7 @@ void archive(const char *path, const char *pkgname) /* {{{ */
 		exit(EXIT_FAILURE);
 	}
 
-	snprintf(dest, PATH_MAX, "%s/%s", pkgname, "foo.txt");
+	snprintf(dest, PATH_MAX, "%s/%s", pkgname, strrchr(path,'/'));
 	copy(path, dest);
 } /* }}} */
 
